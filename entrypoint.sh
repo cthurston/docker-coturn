4608e85ee200232b3f53a76b3b8ae8adeff6f155
Chris <chris.thurston@gmail.com>
2019-10-15 16:38:13 -0700

    Added external ip env var and expose 5349.

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,12 @@ echo "REALM: $REALM"
 echo "PORT RANGE: $MIN_PORT-$MAX_PORT"
 
 internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
-externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+if [ $EXTERNAL_IP -eq 'lookup']; then
+  externalIp=$EXTERNAL_IP
+else
+  externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+fi
 
 echo "listening-port=$LISTEN_PORT
 tls-listening-port=$TLS_LISTEN_PORT
